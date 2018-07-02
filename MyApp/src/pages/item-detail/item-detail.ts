9c0adbf429ec8f5e89f8e99bf574308895ef8bb6
Justin Jetton <justinkjetton@gmail.com>
2018-07-02 00:12:52 -0500

    Modify src/pages/item-detail/item-detail.ts

--- MyApp/src/pages/item-detail/item-detail.ts
+++ MyApp/src/pages/item-detail/item-detail.ts
@@ -1,25 +1,22 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ItemDetailPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
+import { NavParams } from 'ionic-angular';
+ 
 @Component({
   selector: 'page-item-detail',
-  templateUrl: 'item-detail.html',
+  templateUrl: 'item-detail.html'
 })
 export class ItemDetailPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+ 
+  name;
+  status;
+ 
+  constructor(public navParams: NavParams){
+ 
   }
-
+ 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ItemDetailPage');
+    this.name = this.navParams.get('item').name;
+    this.status = this.navParams.get('item').status;
   }
-
+ 
 }
