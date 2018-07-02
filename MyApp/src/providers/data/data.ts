da49804e883fef3ab82b99728d3a7404cb542efd
Justin Jetton <justinkjetton@gmail.com>
2018-07-02 01:39:29 -0500

    Modify data.ts

--- MyApp/src/providers/data/data.ts
+++ MyApp/src/providers/data/data.ts
@@ -1,17 +1,19 @@
-import { HttpClient } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
 import { Injectable } from '@angular/core';
-
-/*
-  Generated class for the DataProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
+ 
 @Injectable()
-export class DataProvider {
-
-  constructor(public http: HttpClient) {
-    console.log('Hello DataProvider Provider');
+export class Data {
+ 
+  constructor(public storage: Storage){
+ 
   }
-
+ 
+  getData() {
+    return this.storage.get('todos'); 
+  }
+ 
+  save(data){
+    this.storage.set('todos', data);
+  }
+ 
 }
