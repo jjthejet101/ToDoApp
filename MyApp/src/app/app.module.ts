c35512ace4e6c786687a78bef5219bbb51a79d77
Justin Jetton <justinkjetton@gmail.com>
2018-07-02 00:07:43 -0500

    Modify src/app/app.module.ts

--- MyApp/src/app/app.module.ts
+++ MyApp/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { AddItemPage } from '../pages/add-item/add-item';
+import { ItemDetailPage } from '../pages/item-detail/item-detail';
  
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    AddItemPage
+    AddItemPage,
+    ItemDetailPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { AddItemPage } from '../pages/add-item/add-item';
   entryComponents: [
     MyApp,
     HomePage,
-    AddItemPage
+    AddItemPage,
+    ItemDetailPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
